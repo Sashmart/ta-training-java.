656874edbbc8042890b6cafcf0b61b4975f412e3
Smart <sashik199@mail.ru>
2022-02-09 20:50:42 +0300

    my homework

---
+++ home/java/src/com/epam/training/student_Sashik/foundamental/main_task/Hellouser.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_Sashik.foundamental.main_task;
+
+public class Hellouser {
+    public static void main(String[] args) {
+        System.out.println("Hello Sashik");
+    }
+}
---
+++ home/java/src/com/epam/training/student_Sashik/foundamental/main_task/Task4.java
@@ -0,0 +1,9 @@
+package com.epam.training.student_Sashik.foundamental.main_task;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String monthsDay []={"31","28","31","30","31","30","31","31","30","31","30","31"};
+        System.out.println("January "+monthsDay[0]+" day");
+
+    }
+}
